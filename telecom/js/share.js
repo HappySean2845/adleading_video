5d54f0fec0ab04bb586dc362d1b5a5ddfd670314
Real <369517224@qq.com>
2017-09-26 00:19:06 +0800

    修改分享

---
+++ telecom/js/share.js
@@ -0,0 +1,73 @@
+$.ajax({
+    url: "http://www.adleading.com/authorize_new/share_sample.php",
+    type: "GET",
+    // cache: true,
+    data: {u: window.location.href},
+    dataType: "jsonp",
+    success: function(back) {
+        wx.config({
+            debug: false,
+            appId: back.appId,
+            timestamp: back.timestamp,
+            nonceStr: back.nonceStr,
+            signature: back.signature,
+            jsApiList: [
+                // 所有要调用的 API 都要加到这个列表中
+                'onMenuShareTimeline',
+                'onMenuShareAppMessage'
+            ]
+        });
+    },
+    error: function() {
+
+    }
+});
+
+wx.ready(function() {
+    // 在这里调用 e
+    wx.error(function(res) {
+        //console.log(res)
+    });
+    //                wx.hideOptionMenu();
+    addWeiXinEvent(0);
+});
+var addWeiXinEvent = function(index) {
+    $.timelineTitle = "深夜，出差的老公给我发了一个视频请求";
+    $.shareAppDesc = "深夜，出差的老公给我发了一个视频请求";
+    $.shareAppTitle = "短短几分钟究竟发生了什么？";
+    $.shareUrl = window.location.href
+    $.shareImage = "http://etude.yiqipaoba.com/util/html/ka/fm.jpg";
+    wx.onMenuShareAppMessage({
+        title: $.shareAppTitle,
+        desc: $.shareAppDesc,
+        link: $.shareUrl,
+        imgUrl: $.shareImage,
+        trigger: function(res) {
+
+        },
+        success: function(res) {
+            //                        _smq.push(['custom', '17-baojun', '730newMB-share']);
+        },
+        cancel: function(res) {
+
+        },
+        fail: function(res) {
+
+        }
+    });
+    wx.onMenuShareTimeline({
+        title: $.timelineTitle,
+        link: $.shareUrl,
+        imgUrl: $.shareImage,
+        trigger: function(res) {
+        },
+        success: function(res) {
+            //                        _smq.push(['custom', '17-baojun', '730newMB-share']);
+        },
+        cancel: function(res) {
+
+        },
+        fail: function(res) {
+        }
+    });
+};
